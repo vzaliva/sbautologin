c30c092ecbae71d37094287cd312a1ca3c093ebc
Igor Giziy <linsalion@codeminders.com>
2011-03-20 08:08:58 +0200

    Added checkbox with saving state

--- src/org/crocodile/sbautologin/MainActivity.java
+++ src/org/crocodile/sbautologin/MainActivity.java
@@ -1,13 +1,31 @@
 package org.crocodile.sbautologin;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+
+    public static final String PREFS_NAME = "sbautologin";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        CheckBox activeChkBox = (CheckBox) findViewById(R.id.active);
+
+        activeChkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("active", isChecked);
+                editor.commit();
+            }
+        });
     }
 }
