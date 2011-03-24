d0bfb5e03c710176cd9c91e94aa9ba37305f66c1
krokodil <krokodil@gmail.com>
2011-03-24 11:27:23 -0700

    Using different event

--- src/org/crocodile/sbautologin/Constants.java
+++ src/org/crocodile/sbautologin/Constants.java
@@ -4,8 +4,10 @@ package org.crocodile.sbautologin;
 public interface Constants
 {
     final int    HIST_LEN        = 10;
-    final String PREFS_NAME      = "sbautologin";
 
+    final String PREFS_NAME      = "sbautologin";
     final String PREF_KEY_ACTIVE = "active";
+
+    final String STARBUCKS_SSID  = "attwifi";
 
 }
