577c610b397a489efef54555dc95826bd95ea4b0
krokodil <krokodil@gmail.com>
2011-03-23 19:59:01 -0700

    Showing table

--- src/org/crocodile/sbautologin/MainActivity.java
+++ src/org/crocodile/sbautologin/MainActivity.java
@@ -1,6 +1,7 @@
 
 package org.crocodile.sbautologin;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -10,9 +11,10 @@ import org.crocodile.sbautologin.model.HistoryItem;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.view.Gravity;
+import android.widget.*;
 
 public class MainActivity extends Activity
 {
@@ -44,11 +46,35 @@ public class MainActivity extends Activity
 
     private void showHistory()
     {
-        DBAccesser db = new DBAccesser(getBaseContext());
+        DBAccesser db = new DBAccesser(this);
         ArrayList<HistoryItem> hist = db.getHistoryItems(4);
+        int i=0;
+        TableLayout histtable = (TableLayout) findViewById(R.id.histTable);
+        histtable.setStretchAllColumns(true);  
+        histtable.setShrinkAllColumns(true);  
         for(HistoryItem h:hist)
         {
+            i++;
             Log.d(TAG,"Read: "+h.getMessage());
+            TableRow row = new TableRow(this); 
+            row.setGravity(Gravity.CENTER_HORIZONTAL);  
+
+            TextView dateCell = new TextView(this);            
+            CharSequence ds = DateUtils.formatSameDayTime(h.getDate().getTime(), 
+                    new Date().getTime(), 
+                    DateFormat.SHORT, 
+                    DateFormat.SHORT);
+            dateCell.setText(ds);
+            dateCell.setGravity(Gravity.LEFT);
+            row.addView(dateCell);
+            
+            TextView msgCell = new TextView(this);
+            msgCell.setText(h.getMessage());
+            msgCell.setGravity(Gravity.LEFT);
+            row.addView(msgCell);
+
+            row.setGravity(Gravity.LEFT);
+            histtable.addView(row);
         }
     }
 
@@ -56,7 +82,7 @@ public class MainActivity extends Activity
     {
         HistoryItem h = new HistoryItem();
 
-        DBAccesser db = new DBAccesser(getBaseContext());
+        DBAccesser db = new DBAccesser(this);
         for(int i = 0; i < 10; i++)
         {
             h.setDate(new Date());
